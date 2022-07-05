2d1e1a29e0708b617fd9d564789f0ba13bc1e492
Ravula Sai Krishna <ravulas@valuefy.com>
2022-07-05 15:32:19 +0530

    first commit

---
+++ pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <Cards id="1" width="200"/> <Cards id="2" width="300"/><Cards id="3" width="400"/><Cards id="4" width="600"/>
+    </div>
+</template>
+
+<script>
+import Cards from './../components/Cards'
+export default {
+    components:{Cards},
+    layout:'login'
+}
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" class="py-5" align="center">
+    <input @input="getValue()"/>
+    {{name}}
+  </v-row>
+</template>
+
+<script>
+export default {
+  layout: 'dashboard',
+  data(){
+    return {
+      name:""
+    }
+  },
+  methods:{
+    getValue(){
+      console.log(this.name)
+    }
+  }
+}
+</script>
